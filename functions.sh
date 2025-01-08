74b69776cb729256fdf9268a95ba9461cfb5c04a
Zu18 <zukhra.tg@gmail.com>
2025-01-08 10:36:35 +0000

    Add the get_user_choice and main

--- functions.sh
+++ functions.sh
@@ -1,7 +1,20 @@
-# Display choice and rules
-
-
-# user_choice4
+# user_choice
+get_user_choice() {
+  while true;
+   do
+    read -p "Enter your choice (1-3): " user_choice
+    if [[ "$user_choice" != 1 && "$user_choice" != 2 && "$user_choice" != 3 ]]; then
+      echo "Invalid choice, please enter a number between 1 and 3."
+    else
+      case $user_choice in
+        1) echo "You chose Rock" ;;
+        2) echo "You chose Paper" ;;
+        3) echo "You chose Scissors" ;;
+      esac
+      break
+    fi
+  done
+}
 
 
 # computer's choice
--- main.sh
+++ main.sh
@@ -1,1 +1,22 @@
+#!/bin/bash
 
+
+source "functions.sh"
+
+main() {
+  while true; 
+  do
+    echo "Rock-Paper-Scissors Games!"
+
+    get_user_choice
+
+
+    read -p "Do you want to play again? (y/n): " play_again
+    if [[ "$play_again" != "y" && "$play_again" != "y" ]]; then
+      echo "Thanks for playing! Goodbye!"
+      break
+    fi
+  done
+}
+
+main
