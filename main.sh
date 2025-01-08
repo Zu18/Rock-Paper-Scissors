6742421ce427c6a23cf171b1ba0f00216af75e89
AdamPalmerGithub <adamdj.palmer@gmail.com>
2025-01-08 10:55:59 +0000

    All files

--- main.sh
+++ main.sh
@@ -9,7 +9,7 @@ main() {
     echo "Rock-Paper-Scissors Games!"
 
     get_user_choice
-
+	computer_choice
 
     read -p "Do you want to play again? (y/n): " play_again
     if [[ "$play_again" != "y" && "$play_again" != "y" ]]; then
@@ -18,5 +18,7 @@ main() {
     fi
   done
 }
+_
+
 
 main
--- messages.sh
+++ messages.sh
@@ -1,9 +1,8 @@
 #stores the responses to the user
-#
+#! /bin/bash
 
+computer_choice()  {
+	random_number=$(( (RANDOM % 3) + 1 ))
 
-
-
-random_number=$(( (RANDOM % 3) + 1 ))
-
-echo "Random number: $random_number"
+	echo "computer choice: $random_number"
+}
