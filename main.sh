48ef30471b4f65e3cd963c8884ede3a71723ce9f
AdamPalmerGithub <adamdj.palmer@gmail.com>
2025-01-09 11:12:48 +0000

    fixed leaving logic

--- main.sh
+++ main.sh
@@ -12,16 +12,24 @@ while true;
     get_user_choice
     computer_choice
     winnerLogic
+	while true; do
+		read -p "Do you want to play again? (y/n): " play_again
+		if [[ "$play_again" == "n" ]]; then
+			display_score_history
+			echo "Thanks for playing! Goodbye!"
+			break 2
 
-    read -p "Do you want to play again? (y/n): " play_again
-    if [[ "$play_again" != "y" && "$play_again" != "y" ]]; then
-      display_score_history
-      echo "Thanks for playing! Goodbye!"
-      break
-    fi
-  done
+		elif [[ "$play_again" == "y" ]]; then
+			echo "Starting new round"
+			echo "------------------"
+			break
+		else
+			echo "Invalid input"
+		fi
+ 	done
+done
+
 }
-
 
 
 main
